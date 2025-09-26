29a8f59e37815661b17451e7560c264574241ebe
Alexa Joy <ajjmarx@umich.edu>
2025-09-26 18:25:54 -0400

    Has frames now

--- js/main.js
+++ js/main.js
@@ -17,7 +17,8 @@ window.addEventListener("DOMContentLoaded", async () => {
 function renderPage(container, data) {
 //        const test = document.createElement("div");
 //        test.innerHTML = findPageFileName(window.location.pathname);
-//        container.appendChild(test);
+//        container.appendChild(test);	
+	customElements.define('special-div', SpecialDiv, { extends: 'div' });
 
         for (const key of Object.keys(data)) {
                 console.log(key);
@@ -47,15 +48,14 @@ function findPageFileName(name) {
 
 function header(container, data) {
         console.log("creating header..");
-        const header = document.createElement("div");
+        const header = document.createElement("div", { is: "special-div" });
         container.appendChild(header);
-        header.style.position = "absolute";
+        header.style.position = "relative";
         header.style.left = "-4px";
         header.style.right = "-4px"
         header.style.top = "-4px";
         header.style.height = "75px";
         header.style.borderRadius = "0px 0px 2em 2em";
-        header.style.border = "4px solid black";
 
         let textDiv;
         for (const item of data) {
@@ -68,11 +68,11 @@ function header(container, data) {
         }
 
         if (textDiv) {
-                textDiv.style.position = "relative";
+                textDiv.style.position = "absolute";
                 textDiv.style.height = "50%";
-                textDiv.style.width = "50%";
+                textDiv.style.width = "75%";
                 textDiv.style.top = "25%";
-                textDiv.style.left = "25%";
+                textDiv.style.left = "12.5%";
                 textDiv.style.textAlign = "center";
                 textDiv.style.verticalAlign= "middle";
                 textDiv.style.lineHeight =  "32px";
@@ -82,38 +82,38 @@ function header(container, data) {
 //                textDiv.style.webkitTextFillColor = "transparent";
         }
 
+	header.reload();
+
         console.log("Header complete");
         return header;
 }
 
 function pageStack(container, data) {
         console.log("creating pageStack..");
-        const box = document.createElement("div");   
+        const box = document.createElement("div", { is: "special-div" });   
         container.appendChild(box);
         box.style.position = "absolute";
         box.style.width = "25%";
-        box.style.left = "4px";
-        box.style.top = "75px"; 
-        box.style.bottom = "4px";
-        box.style.borderRadius = "2em 2em 2em 2em";
-        box.style.border = "4px solid black";
+        box.style.left = "0px";
+        box.style.top = "80px"; 
+        box.style.bottom = "0px";
+	box.reload();
 }
 
 function body(container, data) {
         console.log("creating body..");
-        const body = document.createElement("div");
+        const body = document.createElement("div", { is: "special-div" });   
         container.appendChild(body);
         body.style.position = "absolute";
         body.style.width = "calc(75% - 12px)";
-        body.style.right = "4px";
-        body.style.top = "75px";
-        body.style.bottom = "4px";
-        body.style.borderRadius = "2em 2em 2em 2em";
-        body.style.border = "4px solid black";
+        body.style.right = "0px";
+        body.style.top = "80px";
+        body.style.bottom = "0px";
         for (const item of data) {
                 //console.log(key);
  //               renderFunctions[item["type"]](container, item["data"]);
         }       
+	body.reload();
         console.log("done rendering body");
 }
 
@@ -154,4 +154,102 @@ const renderFunctions = {
 }
 
 
+class SpecialDiv extends HTMLDivElement {
+	constructor() {
+		super();
 
+		this.content = document.createElement("div");
+		this.appendChild(this.content);
+		this.content.style.position = "absolute";
+		this.content.style.width = "calc(100% - 16px)";
+		this.content.style.height = "calc(100% - 16px)";
+		this.content.style.top = "8px";
+		this.content.style.right = "8px";
+		this.content.style.backgroundColor = 'red';
+		
+		this.frame = document.createElement("div");
+		this.appendChild(this.frame);
+		this.frame.style.position = "absolute";
+		this.frame.style.width = "100%";
+		this.frame.style.height = "100%";
+
+		this.parentAppend = this.appendChild.bind(this);
+        	this.appendChild = function(child) {
+			this.content.appendChild(child);
+		}	
+
+		this.reload();
+		window.addEventListener("resize", () => this.reload());
+	}
+
+
+ 
+
+	reload() {
+		this.#render_box(this.clientWidth, this.clientHeight, 9, 3, 150);
+	}	
+
+	#box_svg(width, height, offst, weight, left, top, color) { 	
+		const offset = Math.min(offst, Math.min(width/2, height/2));
+		const handle = 2.2;
+		const right = weight/2;
+		const bottom = weight/2;
+
+		let box = {};
+		
+		box.top = `
+		    <line x1="${offset + left}" y1="${top}" x2="${width - offset - right}" y2="${top}" stroke="${color}" stroke-width="${weight}"/>
+		    <path d="M ${width - right} ${offset + top} C ${width - right} ${offset/handle + top}, ${width - offset/handle - right} ${top}, ${width - offset - right} ${top}" stroke="${color}" stroke-width="${weight}" fill="transparent" />
+		    <path d="M ${left} ${offset + top} C ${left} ${offset/handle + top}, ${offset/handle + left} ${top}, ${offset + left} ${top}" stroke="${color}" stroke-width="${weight}" fill="transparent" />
+		`;
+		 box.bottom = `
+		    <line x1="${offset + left}" y1="${height - bottom}" x2="${width - offset - right}" y2="${height - bottom}" stroke="${color}" stroke-width="${weight}"/>
+		    <path d="M ${width - right} ${height - offset - bottom} C ${width - right} ${height - offset/handle - bottom}, ${width - offset/handle - right} ${height - bottom}, ${width - offset - right} ${height - bottom}" stroke="${color}" stroke-width="${weight}" fill="transparent" />
+		    <path d="M ${left} ${height - offset - bottom} C ${left} ${height - offset/handle - bottom}, ${offset/handle + left} ${height - bottom}, ${offset + left} ${height - bottom}" stroke="${color}" stroke-width="${weight}" fill="transparent" />
+		`;
+		 box.left = `
+		    <line x1="${left}" y1="${offset + top}" x2="${left}" y2="${height - offset - bottom}" stroke="${color}" stroke-width="${weight}"/>
+		    <path d="M ${left} ${offset + top} C ${left} ${offset/handle + top}, ${offset/handle + left} ${top}, ${offset + left} ${top}" stroke="${color}" stroke-width="${weight}" fill="transparent" />
+		    <path d="M ${left} ${height - offset - bottom} C ${left} ${height - offset/handle - bottom}, ${offset/handle + left} ${height - bottom}, ${offset + left} ${height - bottom}" stroke="${color}" stroke-width="${weight}" fill="transparent" />
+		`;
+		 box.right = `
+		    <line x1="${width - right}" y1="${offset + top}" x2="${width - right}" y2="${height - offset - bottom}" stroke="${color}" stroke-width="${weight}"/>
+		    <path d="M ${width - right} ${offset + top} C ${width - right} ${offset/handle + top}, ${width - offset/handle - right} ${top}, ${width - offset - right} ${top}" stroke="${color}" stroke-width="${weight}" fill="transparent" />
+		    <path d="M ${width - right} ${height - offset - bottom} C ${width - right} ${height - offset/handle - bottom}, ${width - offset/handle - right} ${height - bottom}, ${width - offset - right} ${height - bottom}" stroke="${color}" stroke-width="${weight}" fill="transparent" />
+		`;
+		 box.all = `
+		    <line x1="${offset + left}" y1="${top}" x2="${width - offset - right}" y2="${top}" stroke="${color}" stroke-width="${weight}"/>
+		    <line x1="${left}" y1="${offset + top}" x2="${left}" y2="${height - offset - bottom}" stroke="${color}" stroke-width="${weight}"/>
+		    <line x1="${width - right}" y1="${offset + top}" x2="${width - right}" y2="${height - offset - bottom}" stroke="${color}" stroke-width="${weight}"/>
+		    <line x1="${offset + left}" y1="${height - bottom}" x2="${width - offset - right}" y2="${height - bottom}" stroke="${color}" stroke-width="${weight}"/>
+		    <path d="M ${width - right} ${offset + top} C ${width - right} ${offset/handle + top}, ${width - offset/handle - right} ${top}, ${width - offset - right} ${top}" stroke="${color}" stroke-width="${weight}" fill="transparent" />
+		    <path d="M ${left} ${offset + top} C ${left} ${offset/handle + top}, ${offset/handle + left} ${top}, ${offset + left} ${top}" stroke="${color}" stroke-width="${weight}" fill="transparent" />
+		    <path d="M ${width - right} ${height - offset - bottom} C ${width - right} ${height - offset/handle - bottom}, ${width - offset/handle - right} ${height - bottom}, ${width - offset - right} ${height - bottom}" stroke="${color}" stroke-width="${weight}" fill="transparent" />
+		    <path d="M ${left} ${height - offset - bottom} C ${left} ${height - offset/handle - bottom}, ${offset/handle + left} ${height - bottom}, ${offset + left} ${height - bottom}" stroke="${color}" stroke-width="${weight}" fill="transparent" />
+		`;
+
+		return box;
+	}
+
+	#render_box(width, height, offst, weight, angle) {
+		let lab = "oklch(0.75 0.225 ";
+		let blk = this.#box_svg(width - weight/2, height - weight/2, offst - weight/2, weight, weight, weight, "black");
+		let ylw = this.#box_svg(width - weight, height - weight/2, offst - weight/2, weight, weight/2, weight, lab + angle + ")");
+		let grn = this.#box_svg(width - weight/2, height - weight, offst - weight/2, weight, weight, weight/2, lab + (angle+90) + ")");
+		let blu = this.#box_svg(width , height - weight/2, offst - weight/2, weight, weight*1.5, weight, lab + (angle+180) + ")");
+		let red = this.#box_svg(width - weight/2, height, offst - weight/2, weight, weight, weight*1.5, lab + (angle+270) + ")");
+		this.frame.innerHTML = '<svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">' + ylw.left + ylw.right + grn.top + grn.bottom  + blu.left + blu.right + red.top + red.bottom + blk.all + '</svg>';
+	//	console.log(this.innerHTML);
+		//let temp = '<svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">';
+		//for (let i = 0; i < 360; i=i+10) {
+	//		let L = Math.cos(i * 0.0174532925199) * weight/2;
+//			let T = Math.sin(i * 0.0174532925199) * weight/2;
+//			temp = temp + this.#box_svg(width - weight/2 + L, height - weight/2 + T, offst - weight/2, weight, weight + L, weight + T, lab + i + ")").all;
+//		}
+//		
+//		temp += '</svg>';
+//		this.innerHTML = temp;
+//		console.log(this.innerHTML);
+	}
+
+}
