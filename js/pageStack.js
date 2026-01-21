2246c2a11427736a73bf198dc35c7a3f9ee7dac4
Alexa <ajjmarx@umich.edu>
2026-01-21 17:40:47 -0500

    Uses icons

--- js/pageStack.js
+++ js/pageStack.js
@@ -24,16 +24,13 @@ async function addPageStack(container, data, name, isAnimated) {
 
 	let list = [];
 
-	//generate pagelist
 	try {
 		await fetch(data["list"])
 		.then(response => response.text())
 		.then(async text => {
 			lines = text.split(/\n/).filter(line => line.trim() !== '');
 			for (let line of lines) {
-				//console.log(line[0]);
 				if (line[0] == '-') {
-					//console.log(line.substring(1));
 					if (line.indexOf("*") != -1) {
 						let i = 0;
 						console.log(line.substring(1, line.indexOf("*")) + i + line.substring(line.indexOf("*")+1));
@@ -47,18 +44,19 @@ async function addPageStack(container, data, name, isAnimated) {
 								page: "#/" + lineP.substring(6, lineP.length - 5),
 								title : `<p style="font-size: 22px;">` + String(data["meta"]["en_title"]) + "</p>",
 								subtitle : `<p style="color: #575757;">` + String(data["meta"]["en_subtitle"]) + "</p>",
-								star : (data["meta"]["star"] ?? false)
+								tags : (data["meta"]["tags"])
 							});
 							i--;
 						}						
 					} else {
 						const res = await fetch(line.substring(1));
 						const data = await res.json();
+						console.log(data["meta"]);
 						list.push({
 							page: "#/" + line.substring(7, line.length - 5),
 							title : `<p style="font-size: 22px;">` + String(data["meta"]["en_title"]) + "</p>",
 							subtitle : `<p style="color: #575757;">` + String(data["meta"]["en_subtitle"]) + "</p>",
-							star : (data["meta"]["star"] ?? false)
+							tags : (data["meta"]["tags"])
 						});
 					}
 				}
@@ -89,45 +87,55 @@ async function addPageStack(container, data, name, isAnimated) {
 		pg.style.top = tp + "em";
 		pg.style.overflowY = "none";
 		tp = tp + 5.7;
-		//pg.style.padding = "4px 4px 4px 4px";
 		hyperLink.appendChild(pg);
 		const title = document.createElement("div");
 		title.style.position = "absolute";
 		title.style.left = "16px";
-		title.style.top = "8px";
+		title.style.top = "14px";
+		title.style.whiteSpace = "nowrap";
+		title.style.overflow = "hiden";
 		title.innerHTML = list[i]["title"];
 		pg.appendChild(title);
 		const subtitle = document.createElement("div");
 		subtitle.style.position = "absolute";
 		subtitle.style.left = "16px";
-		subtitle.style.top = "38px";
+		subtitle.style.top = "44px";
+		subtitle.style.whiteSpae = "nowrap",
+		subtitle.style.overflow = "hidden",
 		subtitle.innerHTML = list[i]["subtitle"];
 		pg.appendChild(subtitle);
-		if (list[i]["star"]) {
-			const star = document.createElement("div");
-			star.style.position = "absolute";
-			star.style.top = "10px";
-			star.style.right = "16px";
-			star.title = "Important Page";
-			star.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#DA954B"><path d="M782.79-407q20.79-20 48.67-20 27.87 0 47.54 20l53 52q20 21.18 20 48.59Q952-279 931.86-258q-20.15 20-48 20Q856-238 836-258l-53-53q-20-20.4-20-48.2 0-27.8 19.79-47.8ZM827-865.21q20 19.79 20 47.67 0 27.87-20 48.54l-53 53q-19.4 20-47.2 20-27.8 0-48.8-19.79-20-20.79-20-48.67 0-27.87 20-47.54l53-53q20.18-21 48.09-21Q807-886 827-865.21Zm-694.21.21q19.79-21 47.67-21 27.87 0 48.54 21l53 52q20 21.18 20 48.59Q302-737 282.21-716q-20.79 20-48.67 20-27.87 0-47.54-20l-53-53q-20-20.4-20-48.2 0-27.8 19.79-47.8ZM178-407.21q20 19.79 20 47.67 0 27.87-20 48.54l-53 53q-20.4 20-48.2 20Q49-238 29-257.79 9-278.58 9-306.46 9-334.33 29-354l53-53q20.18-20 47.59-20Q157-427 178-407.21ZM411-359l69-43 70 43-19-79 64-55-82.89-8.03L480-576l-31.82 75.92L366-493l64 55-19 79Zm69-108Zm0 224-136 84q-20 12-40.36 10.12-20.35-1.87-35.5-13Q252-174 244.5-192.5T242-233l37-156-123-108q-17-14.06-22-34.03t1-37.97q6-18 21.5-31.5T195-615l160.07-14.3L417-778q9-22 26-32t37-10q20 0 37 10t26 32l61.93 148.7L766-615q23 1 38 14.5t21 31.5q6 18 1.5 38T805-497L682-389l37 157q5 22-2.63 40.37-7.63 18.38-23.37 30.82-15.18 11.07-35.59 12.94Q637-146 617-158l-137-85Z"/></svg>`;	
-			pg.appendChild(star);
+		if (list[i]["tags"]) {
+			const badgeBar = document.createElement("div");
+			badgeBar.style.position = "absolute";
+			badgeBar.style.top = badgeBar.style.right = "8px";
+			badgeBar.style.width = "4em";
+			badgeBar.style.height = "1em";
+			badgeBar.style.zIndex = "1";
+			badgeBar.style.lineHeight = "0px";
+			pg.appendChild(badgeBar);
+			for (tagI in list[i]["tags"]) {
+				const tag = list[i]["tags"][tagI];
+				await addIcon(badgeBar,
+					{
+						"name" : tag,
+						"size" : "small",
+						"color" : convertedCols[(parseInt(tagI) + i) % 8],
+					},
+				0, false);
+			}
 		}
 	}	
 
 	window.addEventListener("resize", () => {
 		if (parseInt(container.offsetWidth, 10) >= 1000) {
-			//box.style.width = (wideWidth) + "px";
-			//box.style.left = wideLeft;
 			box.style.display = "block";
-	//		box.reload();
 		} else if (parseInt(container.offsetWidth, 10) < 1000) {
-			//box.style.width = narrowWidth;
-			//box.style.left = narrowLeft;
 			box.style.display = "none";
-	//		box.reload(); 
 		}
 	});
 }
+
+convertedCols = ["#F579A4", "#F5824A", "#C89C00", "#70B346", "#00B99F", "#00B0E7", "#7E9CFF", "#CC86EB"];
 
 async function removePageStack(element) {
 	if (element) {
