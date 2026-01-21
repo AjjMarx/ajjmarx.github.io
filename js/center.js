454630fe353241fc4e687a97316829adbca25b52
Alexa <ajjmarx@umich.edu>
2026-01-21 17:20:11 -0500

    Fixed generation issues

--- js/center.js
+++ js/center.js
@@ -1,24 +1,59 @@
-function addCenter(container, data, name) {
+function addCenter(container, data, name, isAnimated) {
         //console.log("centering..");
 	const body = document.createElement("div");
 	body.id = assignName(name);
-	//body.style.textAlign = "center";
+	body.style.textAlign = "center";
 	body.style.display = "inline-block";
-	//body.style.width = "100%";
+	body.style.width = "100%";
 	//body.style.height = "100%";	
 	container.appendChild(body);
-	for (const item of data) {
-		const id = item.id ?? 0;
-                spawnFunctions[item["type"]](container, item["data"], parseInt(id,16));
-	}
+	generateChildren(body, data, isAnimated);	
 }
 
-function removeCenter(element) {
-	if (element) { element.remove(); }
+function removeCenter(element, isAnimated) {
+	console.log("removing center");
+	statusHash.set(element.id, "removing");	
+	return new Promise(async (resolve, reject) => {	
+		let content = element;
+		for (child of Array.from(content.children).reverse()) {
+		//	console.log(child.id, child.localName, typeHash.get(parseInt(child.id)));
+			if (typeHash.get(parseInt(child.id)) in supportedFuctions && supportedFuctions[typeHash.get(parseInt(child.id))]) {
+		//		console.log(child);
+				if (isAnimated && child.getBoundingClientRect().top < element.getBoundingClientRect().bottom) {
+					await removalFunctions[typeHash.get(parseInt(child.id))](child, true);
+					console.log("AAAAAv");
+				} else {
+					await removalFunctions[typeHash.get(parseInt(child.id))](child, false);
+				}
+			}
+		}
+
+		statusHash.delete(element.id);
+		element.remove();
+		resolve();
+	});
 }
 
-async function updateCenter(element) {
+async function updateCenter(element, newContent, isAnimated) {
 	console.log("updating center");
-	return;
+	statusHash.set(element.id, "updating");	
+	return new Promise(async (resolve, reject) => {	
+		let content = element.content
+		for (child of Array.from(content.children).reverse()) {
+		//	console.log(child.id, child.localName, typeHash.get(parseInt(child.id)));
+			if (typeHash.get(parseInt(child.id)) in supportedFuctions && supportedFuctions[typeHash.get(parseInt(child.id))]) {
+		//		console.log(child);
+				if (isAnimated && child.getBoundingClientRect().top < element.getBoundingClientRect().bottom) {
+					await removalFunctions[typeHash.get(parseInt(child.id))](child, true);
+				} else {
+					await removalFunctions[typeHash.get(parseInt(child.id))](child, false);
+				}
+			}
+		}
+		await generateChildren(element,newContent, isAnimated)
+
+		statusHash.set(element.id, "idle");	
+		resolve();
+	});
 }
 
